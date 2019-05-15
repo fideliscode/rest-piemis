72df0b4e93391fe4174b2ec65797d07f780f8309
fideliscode <mapendofidelis@gmail.com>
2019-05-15 14:25:14 +0300

    origins

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ mongoose.connect('mongodb+srv://fideliscode:piemispassword@cluster0-9jilr.mongod
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://angular-piemis.herokuapp.com/");
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "*");
   next();
