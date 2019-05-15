337bd98e65121eec79396d5505d6a41b45cccb11
fideliscode <mapendofidelis@gmail.com>
2019-05-16 00:54:44 +0300

    b4 presentation

--- app.js
+++ app.js
@@ -6,22 +6,22 @@ const multipart = require('connect-multiparty');
 const multipartMIddleware = multipart({uploadDir: './uploads'}); 
 const internshipRoutes = require('./api/routes/internships');
 const usersRoutes = require('./api/routes/users');
+const consumer = 'http:127.0.0.1:4200/'
 
+const dburl = 'mongodb+srv://fideliscode:piemispassword@cluster0-9jilr.mongodb.net/test?retryWrites=true';
 
-
-
-mongoose.connect('mongodb+srv://fideliscode:piemispassword@cluster0-9jilr.mongodb.net/test?retryWrites=true',{
-    useNewUrlParser: true
-})
+mongoose.connect(dburl,{ useNewUrlParser: true})
 .then(result=>console.log('connected'))
 .catch(err=>console.log('cannot connect'));
 
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: true}));
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", " * ");
+  res.header("Access-Control-Allow-Headers", " * ");
   next();
 });
 
