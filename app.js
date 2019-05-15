035aacb7d1abb231c698977a9d949db62881ac94
fideliscode <mapendofidelis@gmail.com>
2019-05-16 01:46:33 +0300

    allowed headers

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", " * ");
-  res.header("Access-Control-Allow-Headers", " * ");
+  res.header("Access-Control-Allow-Methods", " POST, GET, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
