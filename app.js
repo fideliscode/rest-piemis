fd60ebc34bd5517d096fed6ac7beb1a60ced7ea4
fideliscode <mapendofidelis@gmail.com>
2019-05-22 13:45:47 +0300

    resolve headers3

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", consumer);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", " POST, GET, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
