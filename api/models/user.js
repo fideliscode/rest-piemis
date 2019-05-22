5d7f7b56188d99a15a78f12000ecdd5c0bdd8d31
fideliscode <mapendofidelis@gmail.com>
2019-05-22 12:42:09 +0300

    resolve header

--- api/models/user.js
+++ api/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 
 const userSchema = mongoose.Schema({
+  _id:mongoose.Schema.Types.ObjectId,
     fname:{type:String,required:true},
     lname:{type:String,required:true},
     email:{type:String,required:true},
--- app.js
+++ app.js
@@ -6,20 +6,21 @@ const multipart = require('connect-multiparty');
 const multipartMIddleware = multipart({uploadDir: './uploads'}); 
 const internshipRoutes = require('./api/routes/internships');
 const usersRoutes = require('./api/routes/users');
-const consumer = 'http:127.0.0.1:4200/'
+// const consumer = 'http:127.0.0.1:4200';
+const consumer='https://angular-piemis.herokuapp.com';
 
 const dburl = 'mongodb+srv://fideliscode:piemispassword@cluster0-9jilr.mongodb.net/test?retryWrites=true';
 
 mongoose.connect(dburl,{ useNewUrlParser: true})
 .then(result=>console.log('connected'))
-.catch(err=>console.log('cannot connect'));
+.catch(err=>console.log('cannot connect now'));
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", consumer);
   res.header("Access-Control-Allow-Methods", " POST, GET, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
