a2063d7704e6ef33dd12952c051fa53013811e5e
FIDELIS MAPENDO <mapendofidelis@gmail.com>
2019-05-30 05:46:00 +0300

    Update app.js
    only one origin allowed

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "consumer1, consumer2");
+  res.header("Access-Control-Allow-Origin", "consumer2");
   res.header("Access-Control-Allow-Methods", " POST, GET, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
