f0f4923bfcaeea3183a2ac16bed6eef5d8763d3b
FIDELIS MAPENDO <mapendofidelis@gmail.com>
2019-11-11 09:43:38 +0300

    removing cors for testing

--- app.js
+++ app.js
@@ -31,12 +31,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
  
  //cors
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", " POST, GET, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", " POST, GET, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 //redirecting to routes
 app.use('/applications', applicationRoutes);
